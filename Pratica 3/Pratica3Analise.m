c14c07c7539d29adbd74ade011c2314ee23a17e1
Larissa de Castro Braga <71854519+larissacb@users.noreply.github.com>
2021-12-10 20:11:58 -0300

    Update Pratica3Analise.m

--- Pratica 3/Pratica3Analise.m
+++ Pratica 3/Pratica3Analise.m
@@ -85,5 +85,44 @@ ylabel("Imag");
 xlabel("Real");
 %%
 %Questao 5
+clc
+clear all
+close all
 
-%Nao sei fazer
+t = (0:0.02:2);
+
+for i = 1:length(t)
+    if (t(i) <= 1)
+        y(i) = 1;
+    else 
+        y(i)= -1;
+    end
+end 
+
+y1 = sin(pi.*t);
+y3 = sin(3*pi.*t)./3;
+y5 = sin(5*pi.*t)./5;
+y7 = sin(7*pi.*t)./7;
+
+figure(1);
+subplot(3,1,1);
+plot(t,y); hold on; 
+plot(t,y1+y3); plot(t,y1+y3+y5+y7);
+axis([0 2 -1.5 1.5]);
+title("Sinal quadrado e seus primeiros harmônicos"); 
+xlabel("Tempo(s)");
+ylabel("y(t)");
+
+subplot(3,1,2);
+plot(t,y1); hold on; plot(t,y3); plot(t,y5); plot(t,y7);
+xlabel("Tempo(s)");
+ylabel("y(t)");
+
+amplitudes = [max(y1) max(y3) max(y5) max(y7)];
+harmonicos = [1 3 5 7]; 
+subplot (3,1,3);
+stem (harmonicos, amplitudes);
+axis([0 8 0 1.5]);
+xlabel("Harmônicos");
+ylabel("Amplitude");
+suptitle("Questão 5");

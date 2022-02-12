d49d133d6cac4cd320db2c13691c9df9f4884d9d
Larissa de Castro Braga <71854519+larissacb@users.noreply.github.com>
2022-02-12 20:31:04 -0300

    Update Pratica10Analise.m

--- Pratica 10/Pratica10Analise.m
+++ Pratica 10/Pratica10Analise.m
@@ -1,7 +1,10 @@
+%Laboratorio de Analise de Sistemas Lineares - Eng. Eletrica
+%Pratica 10 - Modelagem de Sistemas Baseada em Dados - Planta Termica
+%09/02/2022
+%Gabriel Arantes e Larissa Braga
 clc;
 clear all;
 close all;
-
 
 load Dados1_PlantaTermica_P10.txt -ascii;
 
@@ -10,7 +13,7 @@ temp = Dados1_PlantaTermica_P10(:,2); %temperatura
 i = Dados1_PlantaTermica_P10(:,3); %corrente
 
 %%
-%grafico de corrente e temperatura
+%Grafico de corrente e temperatura
 figure(1)
 plot(t, i), grid;
 title('Variação da corrente no tempo');
@@ -24,34 +27,41 @@ xlabel('Tempo (s)');
 ylabel('Temperatura (ºC)');
 
 %%
-%normalização
+%Normalização
 %k = delta(Temp)/delta(t)
-k = (temp(end)- temp(1))/(i(end)-i(1))
+k = (temp(end)- temp(1))/(i(end)-i(1));
 %tau
 y_tau = 0.63*(temp(end)-temp(1))+temp(1);
-%plotando o ponto em que a temperatura vale 63% do valor final
+%Plotando o ponto em que a temperatura vale 63% do valor final
 [a, idx] = min(abs(temp-y_tau));
 x_tau = t(idx)-2; %tau
 hold on
 plot(t(idx),temp(idx), '*'); 
 hold off
 
-theta = 2-1; %diferenca entre o tempo que a temperatura comeca a variar e a aplicacao do degrau
+theta = 2-1; %Diferenca entre o tempo que a temperatura comeca a variar e a aplicacao do degrau
+fprintf('K = %.3f\n', k);
+fprintf('Constante de tempo = %.3f\n', x_tau);
+fprintf('Atraso = %.3f\n', theta);
 
-%funcao de transferencia
+%Funcao de Transferencia
 num = [k];
 den = [x_tau, 1];
 g = tf(num, den, 'iodelay', theta);
 
 %%
-%resposta do sistema ao vetor de corrente dos dados
+%Resposta do sistema ao vetor de corrente dos dados
 gsim = lsim(g, i, t)+ temp(2);
 figure(3)
-plot(t, gsim, 'r'); %resposta ao modelo simulado
+plot(t, gsim, 'r'); %Resposta ao modelo simulado
 hold on
-plot(t, temp, 'k'); %comportamento real
+plot(t, temp, 'k'); %Comportamento real
 grid on
 title('Resposta do sistema');
 xlabel('Tempo(s)');
 ylabel('Temperatura (ºC)');
 legend('Modelo Simulado', 'Modelo Real');
+
+%%
+%Erro medio
+fprintf("Erro médio: %.3f\n", mean(temp-gsim));
